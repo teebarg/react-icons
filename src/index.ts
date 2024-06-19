74d42dec1839d96bd296d26b51e84184dc23633a
Adeniyi <teebarg01@gmail.com>
2024-06-19 21:44:17 +0100

    First commit

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+// src/index.ts
+export { Logo, DiscordIcon } from "./icons";
---
+++ src/types.ts
@@ -0,0 +1,8 @@
+import { SVGProps } from "react";
+
+// eslint-disable-next-line no-undef
+type IconSvgProps = SVGProps<SVGSVGElement> & {
+    size?: number;
+};
+
+export type { IconSvgProps };
