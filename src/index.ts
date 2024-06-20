2560a3240f6a0bde32ae0c8cc74ec9dfcce70ab3
Adeniyi <teebarg01@gmail.com>
2024-06-20 10:54:41 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 // src/index.ts
-export { Logo, DiscordIcon } from "./icons";
+export { Logo } from "./c-icons";
+export { DiscordIcon, TwitterIcon, GithubIcon, FacebookIcon, YoutubeIcon, WhatsAppIcon } from "./icons/social";
+export { MoonFilledIcon, SunFilledIcon, HeartFilledIcon } from "./icons/theme";
