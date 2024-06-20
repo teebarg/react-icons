3d00dc6fccff93226335e022ab32c59778775c8c
Adeniyi <teebarg01@gmail.com>
2024-06-20 12:31:26 +0100

    Update

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@
 export { DiscordIcon, TwitterIcon, GithubIcon, FacebookIcon, YoutubeIcon, WhatsAppIcon } from "./icons/social";
 export { MoonFilledIcon, SunFilledIcon, HeartFilledIcon } from "./icons/theme";
 export { MasterCardIcon, PayPalIcon, VisaCardIcon } from "./icons/payment";
+export * from "./core-icons";
