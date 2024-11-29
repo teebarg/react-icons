606b40ab122c5d00804de92f71d40e50183faa03
Adeniyi <teebarg01@gmail.com>
2024-11-29 17:37:48 +0100

    update

--- src/core-icons.tsx
+++ src/core-icons.tsx
@@ -414,6 +414,14 @@ export const ThumbsUpIcon = ({ height = "1em", width = "1em", ...attributes }: I
     );
 };
 
+export const Tags = ({ height = "1em", width = "1em", ...attributes }: IconSvgProps) => {
+    return (
+        <svg width={width} height={height} {...attributes} stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 640 512">
+            <path d="M497.941 225.941L286.059 14.059A48 48 0 0 0 252.118 0H48C21.49 0 0 21.49 0 48v204.118a48 48 0 0 0 14.059 33.941l211.882 211.882c18.744 18.745 49.136 18.746 67.882 0l204.118-204.118c18.745-18.745 18.745-49.137 0-67.882zM112 160c-26.51 0-48-21.49-48-48s21.49-48 48-48 48 21.49 48 48-21.49 48-48 48zm513.941 133.823L421.823 497.941c-18.745 18.745-49.137 18.745-67.882 0l-.36-.36L527.64 323.522c16.999-16.999 26.36-39.6 26.36-63.64s-9.362-46.641-26.36-63.64L331.397 0h48.721a48 48 0 0 1 33.941 14.059l211.882 211.882c18.745 18.745 18.745 49.137 0 67.882z"></path>
+        </svg>
+    );
+};
+
 export const ThumbsDownIcon = ({ height = "1em", width = "1em", ...attributes }: IconSvgProps) => {
     return (
         <svg width={width} height={height} {...attributes} aria-hidden="true" viewBox="0 0 16 16">
@@ -1006,6 +1014,23 @@ export const ChevronLeft = ({ height = "15", width = "15", ...attributes }: Icon
 export const ChevronLeftMini = ({ height = "15", width = "15", ...attributes }: IconSvgProps) => (
     <svg width={width} {...attributes} height={height} fill="none">
         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M9.722 13.055 4.167 7.5l5.555-5.556"></path>
+    </svg>
+);
+
+export const Clock = ({ height = "1em", width = "1em", ...attributes }: IconSvgProps) => (
+    <svg
+        width={width}
+        {...attributes}
+        height={height}
+        stroke="currentColor"
+        fill="none"
+        strokeWidth="2"
+        viewBox="0 0 24 24"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+    >
+        <circle cx="12" cy="12" r="10"></circle>
+        <polyline points="12 6 12 12 16 14"></polyline>
     </svg>
 );
 
